af17d7d8e6aa9d25fbb3fb4a60d764863dcb7faa
Ativeer <55151895+Ativeer@users.noreply.github.com>
2020-03-18 21:08:53 -0400

    Create question601_humanTraffic.sql

---
+++ question601_humanTraffic.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select distinct * from (select s1.id,s1.visit_date,s1.people from stadium s1, stadium s2, stadium s3 where s1.people>=100 and s2.people>=100 and s3.people>=100 and
+(
+(s1.id = s2.id - 1 and s1.id = s3.id - 2) or
+(s1.id = s2.id - 1 and s1.id = s3.id + 1) or
+(s1.id = s2.id + 1 and s1.id = s3.id + 2)
+)) c1
+ORDER BY c1.id
